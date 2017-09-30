a710f3c415c6d139b24fa7b2babfe8265307dfdd
mim <mimi5025@abv.bg>
2017-09-30 12:45:02 -0700

    try1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def show
-	render html:"enter a"
-	render html:"enter b"
-	
-		
+	render html:"enter a"	
   end
 end
