78f6336721975aeb3b7b97eed6a27553bedcc11a
mim <mimi5025@abv.bg>
2017-10-10 12:07:51 -0700

    sum

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,19 @@ class ApplicationController < ActionController::Base
 	
   end
 
-  
+  def filter
+ file = params[:file].read
+ arr = CSV.parse(file)
+ i = 0
+ sum = 0
+ while i < arr.length do
+     if arr[i][2] % 2 != 0 
+  sum = sum + arr[i][1].to_i
+  end
+  i+=1
+ end
+ render plain: '%.2f'%sum.ceil
+ 
+  end
 end
 
