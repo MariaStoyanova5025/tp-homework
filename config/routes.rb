6e0a57b0067c738070b6b164a1d7ca30a0463631
mim <mimi5025@abv.bg>
2017-10-10 12:42:53 -0700

    i hope im done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	post '/sums' => 'application#sum'
 	post '/filters' => 'application#filter'
 	post '/intervals' => 'application#interval'
+	post '/lin_regressions' => 'application#lin_regressions'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
