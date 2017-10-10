4bd8953ec02a1561efa0081d133f2d5195372bf3
mim <mimi5025@abv.bg>
2017-10-10 12:01:07 -0700

    sum

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	root 'application#show'
 	post '/sums' => 'application#sum'
+	post '/filters' => 'application#filter'
+	post '/interval' => 'application#interval'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
