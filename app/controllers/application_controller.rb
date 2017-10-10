9189941a704713b47f90124bb697c9834afcde5c
mim <mimi5025@abv.bg>
2017-10-10 12:06:22 -0700

    sum

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,39 +16,6 @@ class ApplicationController < ActionController::Base
 	
   end
 
-  def filter
-	file = params[:file].read
-	arr = CSV.parse(file)
-	i = 0
-	sum = 0
-	while i < arr.length do
-   		if arr[i][2] % 2 != 0 
-		sum = sum + arr[i][1].to_i
-		end
-		i+=1
-	end
-	render plain: '%.2f'%sum.ceil
-	
-  end
-  def interval
-	file = params[:file].read
-	arr = CSV.parse(file)
-	i = 0
-	k = 0
-	sum = 0
-	max = 0
-	while i < arr.length - 29 do
-		k = i
-   		while k < 30 do
-			sum = sum + arr[k][0].to_i		
-		end
-		if sum > max
-			max = sum
-		end
-		sum = 0
-		i++
-	end
-	render plain: '%.2f'%max.ceil
-	
-  end
+  
 end
+
